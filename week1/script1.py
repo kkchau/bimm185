c167ac96e6df4e312548b2cfe3dc7f5d8d63f2f1
kkchau <kkhaichau@gmail.com>
2017-04-11 00:22:17 -0700

    script1

---
+++ week1/script1.py
@@ -0,0 +1,25 @@
+import re
+import sys
+
+# command line argument for input file (TCDB.faa)
+filename = sys.argv[1]
+
+# read file
+with open(filename, 'r') as inputfile:
+
+    # output file
+    with open("out.txt", 'w') as out:
+        
+        for i, line in enumerate(inputfile):
+
+            # remove newlines
+            line = line.strip()
+            
+            # check for header
+            if line[0] == '>':
+                if i != 0:
+                    out.write('\n')
+                match = re.match("^>\w+\|\S+\|(.+?)\|(\S+)", line)
+                out.write(match.group(2) + '-' + match.group(1) + '\t')
+            else:
+                out.write(line)
