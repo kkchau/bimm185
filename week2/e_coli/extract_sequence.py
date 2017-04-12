9cd85bfee36ba34815d9a8e0a1c9b087bd66f46a
kkchau <kkhaichau@gmail.com>
2017-04-11 19:46:19 -0700

    week2

---
+++ week2/e_coli/extract_sequence.py
@@ -0,0 +1,39 @@
+import os
+
+comp = {'A': 'T', 'T': 'A', 'G': 'C', 'C': 'G'}
+
+full_seq = ''
+
+with open('GCF_000005845.2_ASM584v2_genomic.fna', 'r') as sequence_file:
+    next(sequence_file)
+    for line in sequence_file:
+        full_seq += line.strip()
+
+fasta = {}
+
+with open('ProteinTable167_161521.txt', 'r') as proteins:
+    next(proteins)
+    for line in proteins:
+        line = line.strip().split()
+        replicon = line[1]
+        strand = line[4]
+        locus = line[6]
+        geneid = line[7]
+        start = int(line[2]) - 1
+        end = int(line[3])
+        sequence = full_seq[start:end]
+        if strand == '-':
+            sequence = list(sequence)[::-1]
+            for i, c in enumerate(sequence):
+                sequence[i] = comp[c]
+            sequence = ''.join(sequence)
+        print(sequence)
+        with open('e_coli.faa', 'a') as fasta:
+            fasta.write('>')
+            fasta.write('|'.join([replicon, locus, geneid]))
+            for counter, character in enumerate(sequence):
+                if counter % 70 == 0:
+                    fasta.write('\n')
+                fasta.write(character)
+            fasta.write('\n')
+
