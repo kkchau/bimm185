8fd3e6658a13896fee3612628fcc8d3d01c649d3
kkchau <kkhaichau@gmail.com>
2017-04-18 19:29:36 -0700

    get random 10 genomes

---
+++ week3/getGenomes.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+rsync -Lvtr rsync://ftp.ncbi.nlm.nih.gov/genomes/all/GCF/000/002/875/GCF_000002875.2_ASM287v2 all_genomes
+rsync -Ltvr rsync://ftp.ncbi.nlm.nih.gov/genomes/all/GCF/000/004/095/GCF_000004095.1_Hydra_RP_1.0
+rsync -Lvtr rsync://ftp.ncbi.nlm.nih.gov/genomes/all/GCF/000/002/985/GCF_000002985.6_WBcel235 all_genomes
+rsync -Lvtr rsync://ftp.ncbi.nlm.nih.gov/genomes/all/GCF/000/002/995/GCF_000002995.3_ASM299v2 all_genomes
+rsync -Lvtr rsync://ftp.ncbi.nlm.nih.gov/genomes/all/GCF/000/003/025/GCF_000003025.5_Sscrofa10.2 all_genomes
+rsync -Lvtr rsync://ftp.ncbi.nlm.nih.gov/genomes/all/GCF/000/003/055/GCF_000003055.6_Bos_taurus_UMD_3.1.1 all_genomes
+rsync -Lvtr rsync://ftp.ncbi.nlm.nih.gov/genomes/all/GCF/000/003/085/GCF_000003085.2_ASM308v2 all_genomes
+rsync -Lvtr rsync://ftp.ncbi.nlm.nih.gov/genomes/all/GCF/000/003/125/GCF_000003125.1_JCVI-TSTA1-3.0 all_genomes
+rsync -Lvtr rsync://ftp.ncbi.nlm.nih.gov/genomes/all/GCF/000/003/135/GCF_000003135.1_ASM313v1 all_genomes
+rsync -Lvtr rsync://ftp.ncbi.nlm.nih.gov/genomes/all/GCF/000/003/195/GCF_000003195.2_Sorbi1 all_genomes
